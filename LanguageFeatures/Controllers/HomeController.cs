f36aa3206747b761bf73d527b68ba17702d300a2
showfella <59898522+showfella@users.noreply.github.com>
2020-03-05 11:34:19 -0800

    Add project files.

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() 
+        {
+            var products = new[] {
+        new { Name = "Kayak", Price = 275M },
+        new { Name = "Lifejacket", Price = 48.95M },
+        new { Name = "Soccer ball", Price = 19.50M },
+        new { Name = "Corner flag", Price = 34.95M }
+        };
+            return View(products.Select(p => $"Name: {p.Name}, Price: {p.Price}"));
+      }
+    }
+}
